a9f87e5658042991f4dd6d4320b296c5667d0879
Tyler Bird <tyler.b.bird@yahoo.com>
2020-04-16 12:28:31 -0500

    Clean up

--- ProjectEulerCpp/ProjectEulerCpp/smallest_multiple.cpp
+++ ProjectEulerCpp/ProjectEulerCpp/smallest_multiple.cpp
@@ -35,20 +35,21 @@ namespace project_euler {
 
     // Prints the least common multiple of the sequence 1 to n
     void print_smallest_multiple_of_one_through_n(const unsigned int n) {
-        std::vector<unsigned int> arr;
+        std::vector<unsigned int> vec;
         for (auto i = 1; i < n; ++i) {
             auto primes = get_prime_factors(i);
             std::vector<unsigned int> result;
             auto new_end = std::set_difference(
-                primes.begin(), primes.end(), arr.begin(), arr.end(),
+                primes.begin(), primes.end(), vec.begin(), vec.end(),
                 std::back_inserter(result)
             );
-            std::move(result.begin(), result.end(), std::back_inserter(arr));
-            std::sort(arr.begin(), arr.end());
+            std::move(result.begin(), result.end(), std::back_inserter(vec));
+            std::sort(vec.begin(), vec.end());
         }
 
+        // Calculate and print the LCM
         unsigned int leastCommonMultiple = 1;
-        for (int i : arr) {
+        for (int i : vec) {
             leastCommonMultiple *= i;
         }
         std::cout << "Least common multiple: " << leastCommonMultiple << "\n";
